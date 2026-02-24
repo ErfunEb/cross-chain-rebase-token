910bde82680cf83aaf532f3449bdcadacbc160d6
Erfan Ebrahimi <prgerfanebrahimi@gmail.com>
2026-02-24 12:47:49 +0330

    Adding CrossChain fork properties

--- src/RebaseTokenPool.sol
+++ src/RebaseTokenPool.sol
@@ -3,18 +3,16 @@ pragma solidity ^0.8.24;
 
 import {TokenPool} from "@ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
 import {Pool} from "@ccip/contracts/src/v0.8/ccip/libraries/Pool.sol";
-import {
-    IERC20
-} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 import {IRebaseToken} from "src/interfaces/IRebaseToken.sol";
 
 contract RebaseTokenPool is TokenPool {
     constructor(
         IERC20 _token,
         address[] memory _allowList,
-        address _rnmProxy,
+        address _rmnProxy,
         address _router
-    ) TokenPool(_token, 18, _allowList, _rnmProxy, _router) {}
+    ) TokenPool(_token, 18, _allowList, _rmnProxy, _router) {}
 
     function lockOrBurn(
         Pool.LockOrBurnInV1 calldata lockOrBurnIn
--- test/unit/CrossChain.t.sol
+++ test/unit/CrossChain.t.sol
@@ -1,12 +1,89 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.30;
+pragma solidity ^0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
+import {IRebaseToken} from "src/interfaces/IRebaseToken.sol";
 import {RebaseToken} from "src/RebaseToken.sol";
 import {RebaseTokenPool} from "src/RebaseTokenPool.sol";
 import {Vault} from "src/Vault.sol";
 import {IRebaseToken} from "src/interfaces/IRebaseToken.sol";
+import {CCIPLocalSimulatorFork, Register} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {RegistryModuleOwnerCustom} from "@ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
+import {IERC20} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {TokenAdminRegistry} from "@ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/TokenAdminRegistry.sol";
 
 contract CrossChain is Test {
-    function setUp() public {}
+    address owner = makeAddr("owner");
+    uint256 sepoliaFork;
+    uint256 arbSepoliaFork;
+
+    CCIPLocalSimulatorFork ccipLocalSimulatorFork;
+    RebaseToken sepoliaToken;
+    RebaseToken arbSepoliaToken;
+
+    Vault vault;
+
+    RebaseTokenPool sepoliaPool;
+    RebaseTokenPool arbSepoliaPool;
+
+    Register.NetworkDetails sepoliaNetworkDetails;
+    Register.NetworkDetails arbSepoliaNetworkDetails;
+
+    function setUp() public {
+        string memory SEPOLIA_ETH_RPC_ENDPOINT = vm.envString(
+            "SEPOLIA_ETH_RPC_ENDPOINT"
+        );
+        string memory ARBITRUM_SEPOLIA_RPC_ENDPOINT = vm.envString(
+            "ARBITRUM_SEPOLIA_RPC_ENDPOINT"
+        );
+        sepoliaFork = vm.createSelectFork(SEPOLIA_ETH_RPC_ENDPOINT);
+        arbSepoliaFork = vm.createFork(ARBITRUM_SEPOLIA_RPC_ENDPOINT);
+
+        ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+        vm.makePersistent(address(ccipLocalSimulatorFork));
+
+        sepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(
+            block.chainid
+        );
+        vm.startPrank(owner);
+        sepoliaToken = new RebaseToken();
+        vault = new Vault(IRebaseToken(address(sepoliaToken)));
+        sepoliaPool = new RebaseTokenPool(
+            IERC20(address(sepoliaToken)),
+            new address[](0),
+            sepoliaNetworkDetails.rmnProxyAddress,
+            sepoliaNetworkDetails.routerAddress
+        );
+        sepoliaToken.grantMintAndBurnRole(address(vault));
+        sepoliaToken.grantMintAndBurnRole(address(sepoliaPool));
+        RegistryModuleOwnerCustom(
+            sepoliaNetworkDetails.registryModuleOwnerCustomAddress
+        ).registerAdminViaOwner(address(sepoliaToken));
+        TokenAdminRegistry(sepoliaNetworkDetails.tokenAdminRegistryAddress)
+            .acceptAdminRole(address(sepoliaToken));
+        TokenAdminRegistry(sepoliaNetworkDetails.tokenAdminRegistryAddress)
+            .setPool(address(sepoliaToken), address(sepoliaPool));
+
+        vm.selectFork(arbSepoliaFork);
+        arbSepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(
+            block.chainid
+        );
+        arbSepoliaToken = new RebaseToken();
+        arbSepoliaPool = new RebaseTokenPool(
+            IERC20(address(arbSepoliaToken)),
+            new address[](0),
+            arbSepoliaNetworkDetails.rmnProxyAddress,
+            arbSepoliaNetworkDetails.routerAddress
+        );
+        arbSepoliaToken.grantMintAndBurnRole(address(vault));
+        arbSepoliaToken.grantMintAndBurnRole(address(arbSepoliaPool));
+        RegistryModuleOwnerCustom(
+            arbSepoliaNetworkDetails.registryModuleOwnerCustomAddress
+        ).registerAdminViaOwner(address(arbSepoliaToken));
+        TokenAdminRegistry(arbSepoliaNetworkDetails.tokenAdminRegistryAddress)
+            .acceptAdminRole(address(arbSepoliaToken));
+        TokenAdminRegistry(arbSepoliaNetworkDetails.tokenAdminRegistryAddress)
+            .setPool(address(arbSepoliaToken), address(arbSepoliaPool));
+        vm.stopPrank();
+    }
 }
